5caf945500891aa9efeb23bd47c28f9d234736e3
sam <samuel.a.ritter.2010@gmail.com>
2020-05-26 20:01:07 -0400

    removing redirect of events

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import EventCalendar from "./components/EventCalendar";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<EventCalendar />, document.getElementById('root'));
 registerServiceWorker();
